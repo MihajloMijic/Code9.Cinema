ed569be5a6f35669486963ad747c33e39b21fc7c
Mihajlo <mihajlomijic01@gmail.com>
2024-05-11 02:40:29 -0700

    Adding cinema done

--- Code9.Domain/Handlers/AddCinemaCommandHandler.cs
+++ Code9.Domain/Handlers/AddCinemaCommandHandler.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace Code9.Domain.Handlers
+using Code9.Domain.Commands;
+using Code9.Domain.Interfaces;
+using Code9.Domain.Models;
+using MediatR;
+
+namespace Code9.Domain.Handlers;
+
+internal class AddCinemaCommandHandler : IRequestHandler<AddCinemaCommand, string>
 {
-    internal class AddCinemaCommandHandler
+    private readonly ICinemaRepository _cinemaRepository;
+
+    public AddCinemaCommandHandler(ICinemaRepository cinemaRepository)
     {
+        _cinemaRepository = cinemaRepository;
+    }
+    
+    public async Task<string> Handle(AddCinemaCommand request, CancellationToken cancellationToken)
+    {
+        var cinema = new Cinema(request.Name, request.City, request.Street, request.NumberOfAuditoriums);
+
+        await _cinemaRepository.Add(cinema, cancellationToken);
+
+        return "Cinema added successfully";
     }
 }
